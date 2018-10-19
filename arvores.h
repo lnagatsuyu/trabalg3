f57342d6be68980031ae1d0bf8ee4530257a776d
lnagatsuyu <leonardo.nagatsuyu@gmail.com>
2018-10-19 17:19:56 -0300

    Update arvores.h

--- arvores.h
+++ arvores.h
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct noB{
+struct noB{				//struct arvore B
 	struct noB *esq, *dir;
 	int chave;
 }
 
-struct noA{
+struct noA{				//struct arvore A
 	struct noA *esq, *pai, *dir;
 	struct noB *chave;
 }
